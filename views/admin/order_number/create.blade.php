a6e00099224d45ea897364ec03ef831cadf12c38
Bob Bloom <info@southlasalle.com>
2016-10-30 13:35:47 -0400

    Continuing work on the custom admin forms. #5

--- views/admin/order_number/create.blade.php
+++ views/admin/order_number/create.blade.php
@@ -20,9 +20,69 @@
 
             <br /><br />
 
-<h1>hello from admin/order_number/create!!!!!!!</h1>
+            <div class="row">
+
+                @include('lasallecmsadmin::bob1.partials.message')
+
+                <div class="col-md-3"></div>
+
+                <div class="col-md-6">
 
 
+
+                </div> <!-- col-md-6 -->
+                <div class="col-md-3"></div>
+
+
+                    {{-- this is a combo create or edit form. Display the proper "form action"  --}}
+                    @if ( isset($orderNumber) )
+                        {!! Form::model($orderNumber, array('route' => array('admin.customordernumber.update', $orderNumber->id), 'method' => 'PUT')) !!}
+
+                        {!! Form::hidden('id', $orderNumber->id) !!}
+                    @else
+                        {!! Form::open(['route' => 'admin.customordernumber.store']) !!}
+                    @endif
+
+                    {{-- the table! --}}
+                    <table class="table table-striped table-bordered table-condensed table-hover">
+
+                        <tr>
+                            <td>
+
+                            </td>
+                            <td>
+                                @if ( isset($orderNumber) )
+                                    {!! Form::submit( 'Edit Order Number!') !!}
+                                @else
+                                    {!! Form::submit( 'Create Order Number!') !!}
+                                @endif
+
+                                {!! $HTMLHelper::back_button('Cancel') !!}
+                            </td>
+                        </tr>
+                        <tr><td colspan="2"><hr></td></tr>
+
+                        <tr>
+                            <td>
+                                {!! Form::label('order_number', 'Order Number: ') !!}
+                            </td>
+                            <td>
+
+                                @if ( isset($orderNumber) )
+                                    {!! Form::test('email', Input::old('order_number', isset($orderNumber) ? $orderNumber->order_number : '')) !!}&nbsp;&nbsp; <a href="#" data-toggle="popover" data-content="Order Number must be unique."><i class="fa fa-info-circle"></i></a>
+                                    {{{ $errors->first('order_number', ':message') }}}
+                                @else
+                                    <input type="text" name="order_number" value="{{{ $orderNumber->order_number }}}" disabled>
+                                @endif
+                            </td>
+                        </tr>
+
+
+                        <input type="hidden" name="title" value="{{{ $orderNumber->title }}}">
+
+                        </table>
+
+            </div> <!-- row -->
 
         </div> <!-- container -->
 
